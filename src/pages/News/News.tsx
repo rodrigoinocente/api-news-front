990c27938892b4d91589aebcd39ab2bd0f829709
Rodrigo Inocente <ridrigocorreia@gmail.com>
2024-12-09 15:23:06 -0300

    feat: fetch and render news from the database with initial styling
    - Updated the News page to fetch and render news from the database.
    
    - Added initial styling to improve visual presentation.

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -1,12 +1,50 @@
 import { useParams } from "react-router-dom"
+import { NewsBoby, NewsContent, NewsHead } from "./NewsStyled";
+import { getNewsById } from "../../service/newsService";
+import { useEffect, useState } from "react";
+import { INews } from "../../vite-env";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { NavbarHome } from "../../components/NavBarHome/NavBarHome";
+import { useBackground } from "../../Context/useBackgroundCustomHook";
 
 export function News() {
-    const { newsId } = useParams()
-    console.log("NEWSID: ", newsId);
+    const { newsId } = useParams<{ newsId: string }>()
+    const [news, setNews] = useState<INews | null>(null)
+    const { updateBackground } = useBackground()
+
+    const findNews = async () => {
+        if (!newsId) return console.error("newsId is undefined")
+
+        try {
+            const newsResponse = await getNewsById(newsId)
+            setNews(newsResponse.data)
+        } catch (error) {
+            console.error("Erro ao carregar notícias:", error)
+        }
+    }
+
+    useEffect(() => {
+        updateBackground(null)
+        findNews();
+    }, [newsId]);
 
     return (
         <>
-            <h1>HELLO, WORD!</h1>
+            <Navbar />
+            <NavbarHome />
+            {news && (
+                <NewsBoby>
+                    <NewsHead>
+                        <h1>{news.title}</h1>
+                        <p className="subtitle">{news.subtitle}</p>
+                        <p className="author">Por: {news.authorId.name}</p>
+                        <img src={news.banner} alt="Imagem da notícia" />
+                    </NewsHead>
+                    <NewsContent>
+                        <div dangerouslySetInnerHTML={{ __html: news.content }} />
+                    </NewsContent>
+                </NewsBoby>
+            )}
         </>
     )
 }
---
+++ src/pages/News/NewsStyled.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+export const NewsBoby = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    width: 70%;
+    margin: 0 auto ;
+    margin-top: 2rem;
+    line-height: 1.5;
+`
+
+export const NewsHead = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+    width: 80%;
+    
+    h1 {
+        line-height: 1.3; 
+        align-self: self-start;
+    }
+
+    .subtitle {
+        font-size: .9rem;
+        color: #3a3939;
+    }
+
+    img {
+        width: 100%;
+        margin: 2rem 0 2rem ;
+    }
+    
+`
+
+export const NewsContent = styled.div`
+    div {
+        display: flex;
+        flex-direction: column;
+        margin: 0 auto;
+        gap: 1.5rem;
+        width: 80%;
+
+        p {
+            font-size: 1.1rem;
+        }
+    }
+    
+`
