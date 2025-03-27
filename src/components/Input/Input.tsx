18605ba58e004da805572a90f06986b0932d0571
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-03-26 23:02:37 -0300

    feat(input): enhance Input component with accessibility props
    - Add 'id' prop for better form accessibility.
    
    - Include autoComplete prop to support browser autofill.
    
    - Update TypeScript interface and component implementation.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,20 @@
 import { UseFormRegister } from "react-hook-form";
 import { InputSpaceAuth } from "./InputStyled"
-import { AuthData} from "../../vite-env";
+import { AuthData } from "../../vite-env";
 
 interface IInputSpaceAuth {
     type: string;
     placeholder: string;
-    register: UseFormRegister<AuthData> ;
+    register: UseFormRegister<AuthData>;
     name: keyof AuthData;
+    id: string;
+    autoComplete?: string;
 }
 
-export function Input({ type, placeholder, register, name }: IInputSpaceAuth) {
+export function Input({ type, placeholder, register, name, id, autoComplete }: IInputSpaceAuth) {
     return (
         <>
-            <InputSpaceAuth type={type} placeholder={placeholder} {...register(name)} />
+            <InputSpaceAuth type={type} placeholder={placeholder} {...register(name)} id={id} autoComplete={autoComplete} />
         </>
     )
 }
