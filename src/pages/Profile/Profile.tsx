6f28d2ae12df68010ae9a237efb63d28a0ee22d4
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-04-01 20:32:19 -0300

    feat(profile): add inline editing toggles and refactor update forms
    - Implemented toggle functionality for updating profile picture, name, email, and password.
    
    - Integrated icon buttons (edit, confirm, cancel) for a clearer inline editing experience.
    
    - Improved error handling and form reset behavior after successful updates.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,15 +5,17 @@ import { updateUser, userLogged } from "../../service/userService";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { UserAvatar } from "../../components/UserAvatar/UserAvatar";
 import axios from "axios";
-import { ProfileContainer } from "./ProfileStyled";
+import { IconCancel, IconConfirm, IconEdit, ProfileContainer } from "./ProfileStyled";
 import { useForm } from "react-hook-form";
 import { AuthData } from "../../vite-env";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { updateProfilePictureSchema, updateEmailSchema, updateNameSchema, updatePasswordSchema } from "../../schemas/updateUser";
-import { SectionForm } from "../../components/Modals/ModalStyled";
+import { SectionForm } from "./ProfileStyled";
 import { Input } from "../../components/Input/Input";
 import { ErrorSpan } from "../../components/Navbar/NavbarStyled";
-import { Button } from "../../components/Button/Button";
+import iconEdit from "../../images/icons/icon-edit.png";
+import iconConfirm from "../../images/icons/icon-confirm.png";
+import iconCancel from "../../images/icons/icon-cancel.png";
 
 export function Profile() {
     const { user, setUser } = useUser()
@@ -22,6 +24,10 @@ export function Profile() {
     const [emailError, setEmailError] = useState<string | null>(null)
     const [updateResults, setUpdateResults] = useState<string | null>(null)
     const [networkError, setNetworkError] = useState<string | null>(null)
+    const [isEditingName, setIsEditingName] = useState(false)
+    const [isEditingEmail, setIsEditingEmail] = useState(false)
+    const [isEditingPassword, setIsEditingPassword] = useState(false)
+    const [isEditingProfilePicture, setIsEditingProfilePicture] = useState(false)
 
     const {
         register: registerUpdateProfilePicture,
@@ -68,14 +74,15 @@ export function Profile() {
         initializeUser()
     }, [setUser])
 
-    async function onUpdateSubmit(data: Partial<AuthData>, reset: () => void) {
+    async function onUpdateSubmit(data: Partial<AuthData>, reset: () => void,
+        setIsEditing: React.Dispatch<React.SetStateAction<boolean>>) {
         try {
             const response = await updateUser(userId, data)
             upDateLocalStorage(response.data.user)
             setUser(constructUserFromLocalStorage())
             setUpdateResults(response.data.message)
             reset()
-
+            setIsEditing(false)
         } catch (error: unknown) {
             if (axios.isAxiosError(error)) {
                 setEmailError(error.response?.data.message)
@@ -93,10 +100,9 @@ export function Profile() {
                 <h3>{loginError}</h3>
             ) : (
                 <ProfileContainer>
-
                     {user ? (
                         <>
-                            <SectionForm>{/* TODO: PROVISIONAL, REMOVE WHEN MAKE STYLE */}
+                            <SectionForm>
                                 {updateResults &&
                                     <h3>{updateResults}</h3>
                                 }
@@ -105,54 +111,151 @@ export function Profile() {
                                 }
 
                                 <h1>Conta</h1>
-                                <UserAvatar user={user} />
-                                <p>Atualizar foto de Perfil</p>
+                                <UserAvatar user={user} size="7rem" />
 
-                                <form onSubmit={handleSubmitupdateProfilePicture((data) => onUpdateSubmit(data, resetUpdateProfilePicture))}>
-                                    <Input type="text" placeholder="Insira a URL da imagem" name="profilePicture" register={registerUpdateProfilePicture} />
-                                    {errorsupdatProfilePicture.profilePicture && <ErrorSpan>{errorsupdatProfilePicture.profilePicture.message}</ErrorSpan>}
-                                    <Button type="submit" text="Atualizar" />
-                                </form>
+                                {!isEditingProfilePicture ? (
+                                    <>
+                                        <IconEdit className="icon-update-picture" onClick={() => setIsEditingProfilePicture(true)}>
+                                            <img src={iconEdit} alt="Ícone para atualizar a foto de perfil" />
+                                        </IconEdit>
+                                    </>
+                                ) : (
+                                    <form onSubmit={handleSubmitupdateProfilePicture((data) => onUpdateSubmit(data, resetUpdateProfilePicture, setIsEditingProfilePicture))}>
+                                        <label htmlFor="profilePicture">Atualizar foto de Perfil</label>
+                                        <Input type="text" placeholder="Insira a URL da imagem" name="profilePicture" id="profilePicture" register={registerUpdateProfilePicture} />
+                                        {errorsupdatProfilePicture.profilePicture && <ErrorSpan>{errorsupdatProfilePicture.profilePicture.message}</ErrorSpan>}
 
-                                <form onSubmit={handleSubmitupdateName((data) => onUpdateSubmit(data, resetUpdateName))}>
-                                    <p><span>Nome: </span>{user.name}</p>
-                                    <Input type="text" placeholder={user.name} name="name" register={registerUpdateName} />
-                                    {errorsupdateName.name && <ErrorSpan>{errorsupdateName.name.message}</ErrorSpan>}
-                                    <Button type="submit" text="Atualizar" />
-                                </form>
+                                        <IconConfirm typeof="submit">
+                                            <button type="submit">
+                                                <img src={iconConfirm} alt="Ícone para atualizar o nome do usuario" />
+                                            </button>
+                                        </IconConfirm>
 
-                                <form onSubmit={handleSubmitupdateEmail((data) => onUpdateSubmit(data, resetUpdateUpdateEmail))}>
-                                    <p><span>Email: </span>{user.email}</p>
+                                        <IconCancel>
+                                            <button type="button" onClick={() => {
+                                                setIsEditingProfilePicture(false)
+                                                resetUpdateProfilePicture()
+                                            }}>
+                                                <img src={iconCancel} alt="Ícone para cancelar a atualização" />
+                                            </button>
+                                        </IconCancel>
+                                    </form>
+                                )}
 
-                                    <Input type="email" placeholder={user.email} name="email" register={registerUpdateEmail} />
-                                    {errorsupdateEmail.email && <ErrorSpan>{errorsupdateEmail.email.message}</ErrorSpan>}
-                                    {emailError && <ErrorSpan>{emailError}</ErrorSpan>}
-                                    <Button type="submit" text="Atualizar" />
-                                </form>
+                                {!isEditingName ? (
+                                    <>
+                                        <span>Nome: {user.name}</span>
+                                        <IconEdit onClick={() => setIsEditingName(true)}>
+                                            <img src={iconEdit} alt="Ícone para atualizar a foto de perfil" />
+                                        </IconEdit>
+                                    </>
+                                ) : (
+                                    <form onSubmit={handleSubmitupdateName((data) => onUpdateSubmit(data, resetUpdateName, setIsEditingName))}>
+                                        <label htmlFor="name">Nome: </label>
+                                        <Input type="text" placeholder={user.name} name="name" id="name" autoComplete="name" register={registerUpdateName} />
+                                        {errorsupdateName.name && <ErrorSpan>{errorsupdateName.name.message}</ErrorSpan>}
 
-                                <form onSubmit={handleSubmitupdatePassword((data) => onUpdateSubmit(data, resetUpdateUpdatePassword))}>
-                                    <p>Atualizar Senha</p>
-                                    <Input type="password" placeholder="Senha" name="password" register={registerUpdatePassword} />
-                                    {errorsupdatePassword.password && <ErrorSpan>{errorsupdatePassword.password.message}</ErrorSpan>}
+                                        <IconConfirm typeof="submit">
+                                            <button type="submit" >
+                                                <img src={iconConfirm} alt="Ícone para atualizar o nome do usuario" />
+                                            </button>
+                                        </IconConfirm>
 
-                                    <Input type="password" placeholder="Confirmar senha" name="confirmPassword" register={registerUpdatePassword} />
-                                    {errorsupdatePassword.confirmPassword && <ErrorSpan>{errorsupdatePassword.confirmPassword.message}</ErrorSpan>}
+                                        <IconCancel>
+                                            <button type="button" onClick={() => {
+                                                setIsEditingName(false)
+                                                resetUpdateName()
+                                            }}>
+                                                <img src={iconCancel} alt="Ícone para cancelar a atualização" />
+                                            </button>
+                                        </IconCancel>
+                                    </form>
+                                )}
 
-                                    <Button type="submit" text="Atualizar" />
-                                </form>
+                                {!isEditingEmail ? (
+                                    <>
+                                        <span>Email: {user.email}</span>
+
+                                        <IconEdit onClick={() => {
+                                            setIsEditingEmail(true)
+                                        }}>
+                                            <img src={iconEdit} alt="Ícone para atualizar a foto de perfil" />
+                                        </IconEdit>
+                                    </>
+                                ) : (
+                                    <form onSubmit={handleSubmitupdateEmail((data) => onUpdateSubmit(data, resetUpdateUpdateEmail, setIsEditingEmail))}>
+                                        <label htmlFor="email">Email: </label>
+                                        <Input type="email" placeholder={user.email} name="email" id="email" autoComplete="email" register={registerUpdateEmail} />
+                                        {errorsupdateEmail.email && <ErrorSpan>{errorsupdateEmail.email.message}</ErrorSpan>}
+                                        {emailError && <ErrorSpan>{emailError}</ErrorSpan>}
+
+                                        <IconConfirm typeof="submit">
+                                            <button type="submit">
+                                                <img src={iconConfirm} alt="Ícone para atualizar o nome do usuario" />
+                                            </button>
+                                        </IconConfirm>
+
+                                        <IconCancel>
+                                            <button type="button" onClick={() => {
+                                                setIsEditingEmail(false)
+                                                resetUpdateUpdateEmail()
+                                            }}>
+                                                <img src={iconCancel} alt="Ícone para cancelar a atualização" />
+                                            </button>
+                                        </IconCancel>
+                                    </form>
+                                )}
+
+                                {!isEditingPassword ? (
+                                    <h5 onClick={() => {
+                                        setIsEditingPassword(true)
+                                    }}>Atualizar Senha</h5>
+                                ) : (
+                                    <>
+                                        <h5>Cadastrar nova senha</h5>
+                                        <form onSubmit={handleSubmitupdatePassword((data) => onUpdateSubmit(data, resetUpdateUpdatePassword, setIsEditingPassword))}>
+                                            <label htmlFor="password">Digite a nova senha </label>
+                                            <Input type="password" placeholder="Senha" name="password" id="password" autoComplete="new-password" register={registerUpdatePassword} />
+                                            {errorsupdatePassword.password && <ErrorSpan>{errorsupdatePassword.password.message}</ErrorSpan>}
+
+                                            <label htmlFor="confirmPassword">Digite novamente a nova senha </label>
+                                            <Input type="password" placeholder="Confirmar senha" name="confirmPassword" id="confirmPassword" autoComplete="new-password" register={registerUpdatePassword} />
+                                            {errorsupdatePassword.confirmPassword && <ErrorSpan>{errorsupdatePassword.confirmPassword.message}</ErrorSpan>}
+
+                                            <IconConfirm typeof="submit">
+                                                <button type="submit">
+                                                    <img src={iconConfirm} alt="Ícone para atualizar o nome do usuario" />
+                                                </button>
+                                            </IconConfirm>
+
+                                            <IconCancel>
+                                                <button type="button" onClick={() => {
+                                                    setIsEditingPassword(false)
+                                                    resetUpdateUpdatePassword()
+                                                }}>
+                                                    <img src={iconCancel} alt="Ícone para cancelar a atualização" />
+                                                </button>
+                                            </IconCancel>
+                                        </form>
+                                    </>
+                                )}
 
                                 {loginError &&
                                     <h3>{loginError}</h3>
                                 }
-
                             </SectionForm>
                         </>
                     ) : (
                         <h3>Sem Usuário</h3>
                     )}
                 </ProfileContainer>
-            )
-            }
+            )}
         </>
     )
 }
+
+//TODO=> VALIDAR O E-MAIL EM TEMPO DE DIGITAÇÃO, SEM PRECISAR FAZER O SUBMIT
+//      HTML AJUSTADO OS FORMULARIOS ESTÃO FUNCIONANDO, FALTA ADICIONAR A ESTILIZAÇÃO.
+//      O EFEITO DO INPU DO NAME ESTÁ IMPLEMENTADO O EFEITO VISUAL
+
+//TODO: PREPARA MENSAFGEM DE COMMIT DAS ALTERAÇÕES DA PAGINA PROFILE
--- src/pages/Profile/ProfileStyled.tsx
+++ src/pages/Profile/ProfileStyled.tsx
@@ -5,8 +5,51 @@ export const ProfileContainer = styled.div`
     justify-content: center;
     margin-top: 4rem;
     text-align: center;
+
+    .icon-update-picture {
+        position: relative;
+        bottom: 30px;
+        left: 30px;
+        cursor: pointer;
+    }
+
+    button {
+        background: none;
+        border: none;
+    }
 `
 
 export const SectionForm = styled.section`
     
+`
+
+export const IconEdit = styled.section`
+    img {
+        background:#6d84eb;
+        border-radius: 50%;
+        width: 2rem;
+        padding: 5px;
+        cursor: pointer;
+    }
+`
+
+export const IconConfirm = styled.section`
+    img {
+        background:#04e904;
+        border-radius: 50%;
+        width: 2rem;
+        padding: 4px;
+        cursor: pointer;
+    }
+`
+
+export const IconCancel = styled.section`
+    img {
+        background: #fa5555;
+        border-radius: 50%;
+        width: 2rem;
+        height: 2rem;
+        padding: 4px;
+        cursor: pointer;
+    }
 `
