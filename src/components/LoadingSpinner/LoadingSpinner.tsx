65e229cc132cdc608fb01078d80f8fc08514deb7
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-06-30 20:42:48 -0300

    refactor(spinner): improve rendering and user experience
    - Adjust Spinner component implementation for enhanced usability.

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -3,6 +3,5 @@ import { SpinnerWrapper } from "./LoadingSpinnerStyled";
 export const Spinner = () => (
     <SpinnerWrapper>
         <div></div>
-        <p>No momento o back-end está em fase de desenvolvimento. Talvez demore alguns segundos.</p>
     </SpinnerWrapper>
 )
--- src/components/LoadingSpinner/LoadingSpinnerStyled.tsx
+++ src/components/LoadingSpinner/LoadingSpinnerStyled.tsx
@@ -3,18 +3,17 @@ import styled from "styled-components";
 export const SpinnerWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1rem;
     align-items: center;
-    padding: 5rem;
+    padding-top: 5rem;
     height: 100vh;
 
     div {
         border: 8px solid rgba(0, 0, 0, 0.1);
         border-radius: 50%;
-        border-left-color: #3498db; /* Cor do spinner */
-        width: 50px;
-        height: 50px;
-        animation: spin 1s linear infinite; /* Animação de rotação */
+        border-left-color: #3498db;
+        width: 80px;
+        height: 80px;
+        animation: spin 1s linear infinite;
     }
 
     @keyframes spin {
