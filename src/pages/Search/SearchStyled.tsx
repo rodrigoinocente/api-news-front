82f8f62101a6c1aa090168bb6ce351c4ce35e821
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-06-30 20:39:04 -0300

    style(search): refactor styles for mobile-first responsiveness

--- src/pages/Search/SearchStyled.tsx
+++ src/pages/Search/SearchStyled.tsx
@@ -1,10 +1,18 @@
 import styled from "styled-components"
 
+export const Container = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    justify-self: center;
+    width: 95%;
+    gap: 1rem;
+    margin: 1rem 0;
+`
+
 export const TextResults = styled.section`
-    padding-top: 1rem;
     
     h3{
-        margin: 0 0 2rem 17rem;
+        word-wrap: break-word;
 
         span{
             color: #807e7e;
@@ -16,12 +24,6 @@ export const TextResults = styled.section`
 `
 export const ContainerResults = styled.section`
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    `
-export const SearchNews = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 60%;
-    gap:1rem
+    flex-flow: column nowrap;
+    gap: 2rem;
 `
