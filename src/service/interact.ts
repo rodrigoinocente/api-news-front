92543283e3667787a64e68fe586a7378fa517d3c
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-07-07 14:29:12 -0300

    feat(service): add 'interact' module for comment and reply API communication
    - Introduce 'interact.ts' in services to centralize all HTTP calls related to comments and replies.
    
    - Implement 'getComments' for paginated fetch of comments by 'newsId' (with 'limit' and 'offset').
    
    - Implement 'sendComment' to post new comments to the backend with credentials support.

---
+++ src/service/interact.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_API_INTERACT_URL
+
+export interface ISendComment {
+    content: string;
+}
+
+export function getComments(newsId: string, limit: number, offset: number) {
+    const response = axios.get(`${baseUrl}/comment/commentPage/${newsId}`, { params: { limit, offset }, withCredentials: true })
+    return response
+}
+
+export function sendComment(newsId: string, data: ISendComment) {
+    const response = axios.post(`${baseUrl}/comment/${newsId}`, data, { withCredentials: true })
+    return response
+}
