d3dcd9ccb139a738b0331b0d78398b1fcf0cedc6
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-07-11 11:48:05 -0300

    feat(comment): enable comment like and delete actions in CommentItem
    CommentItem to support like and delete interactions via props.
    
    Added visual feedback for liked comments with dynamic icon.

--- src/components/InteractComponents/CommentItem/CommentItem.tsx
+++ src/components/InteractComponents/CommentItem/CommentItem.tsx
@@ -2,17 +2,19 @@ import { ApiCommentData } from "../../../vite-env";
 import { NewsTimestamps } from "../../NewsTimestamps/NewsTimestamps";
 import { UserAvatar } from "../../UserAvatar/UserAvatar";
 import iconLike from "../../../images/icons/icon-like.png";
+import iconLikeCheck from "../../../images/icons/icon-likeCheck.png";
 import iconComments from "../../../images/icons/icon-comments.png";
 import iconTrash from "../../../images/icons/icon-trash.png";
 import { Container, Footer, Header } from "./CommentItemStyled";
 import { useUser } from "../../../Context/userCustomHook";
 
-	interface CommentItemProps {
-  comment: ApiCommentData;
-  onDelete: (dataCommentId: string, commentId: string) => void;
+interface CommentItemProps {
+	comment: ApiCommentData;
+	onDeleteComment: (dataCommentId: string, commentId: string) => void;
+	onLikeComment: (dataCommentId: string, commentId: string) => void;
 }
 
-export function CommentItem({ comment, onDelete }: CommentItemProps) {
+export function CommentItem({ comment, onDeleteComment, onLikeComment }: CommentItemProps) {
 	const { user, } = useUser()
 	const isOwner = user?._id === comment.user._id;
 
@@ -30,7 +32,7 @@ export function CommentItem({ comment, onDelete }: CommentItemProps) {
 				</div>
 
 				{isOwner &&
-					<div id="trash" onClick={()=>onDelete(comment.documentId, comment._id)}>
+					<div id="trash" onClick={() => onDeleteComment(comment.documentId, comment._id)}>
 						<img src={iconTrash} alt="Ícone para exclusão do comentário" />
 					</div>
 				}
@@ -42,8 +44,12 @@ export function CommentItem({ comment, onDelete }: CommentItemProps) {
 
 			<Footer>
 				<div id="counts">
-					<div className="countsIcon">
-						<img src={iconLike} alt="Ícone para curtir o comentário" />
+					<div className="countsIcon" onClick={() => onLikeComment(comment.documentId, comment._id)}>
+						{!comment.isLiked ?
+							<img src={iconLike} alt="Ícone para curtir o comentário" />
+							:
+							<img src={iconLikeCheck} alt="Ícone do comentário curtido" />
+						}
 						<span>{comment.likeCount}</span>
 					</div>
 
