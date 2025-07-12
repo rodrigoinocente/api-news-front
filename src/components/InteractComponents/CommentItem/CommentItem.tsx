33d2fd3c56ecd2525b8fd435f8e8541dd0029fdd
Rodrigo Inocente <ridrigocorreia@gmail.com>
2025-07-12 13:41:26 -0300

    feat(comment): integrate reply form and submission handling into CommentItem

--- src/components/InteractComponents/CommentItem/CommentItem.tsx
+++ src/components/InteractComponents/CommentItem/CommentItem.tsx
@@ -7,6 +7,14 @@ import iconComments from "../../../images/icons/icon-comments.png";
 import iconTrash from "../../../images/icons/icon-trash.png";
 import { Container, Footer, Header } from "./CommentItemStyled";
 import { useUser } from "../../../Context/userCustomHook";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { interactSchema } from "../../../schemas/interactSchema";
+import { z } from "zod";
+import { sendReply } from "../../../service/interact";
+import { Button } from "../../Button/Button";
+import axios from "axios";
 
 interface CommentItemProps {
 	comment: ApiCommentData;
@@ -14,9 +22,39 @@ interface CommentItemProps {
 	onLikeComment: (dataCommentId: string, commentId: string) => void;
 }
 
+type IReply = z.infer<typeof interactSchema>;
+
 export function CommentItem({ comment, onDeleteComment, onLikeComment }: CommentItemProps) {
-	const { user, } = useUser()
+	const { user } = useUser()
 	const isOwner = user?._id === comment.user._id;
+	const [showReplyInput, setShowReplyInput] = useState(false)
+	const [isSendReply, setIsSendReply] = useState(false)
+	const [replyError, setReplyError] = useState<string | null>(null)
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset
+	} = useForm<IReply>({ resolver: zodResolver(interactSchema) })
+
+	const inHandleSubmit = async (data: IReply) => {
+		setIsSendReply(true)
+		try {
+			const response = await sendReply(comment.documentId, comment._id, data)
+			console.log("reponseSenReply: ", response);
+			reset()
+			setShowReplyInput(false)
+
+
+		} catch (error: unknown) {
+			if (axios.isAxiosError(error)) setReplyError(error.response?.data.message || "Ocorreu um erro desconhecido")
+			else setReplyError("Ocorreu um erro desconhecido")
+			console.error(error)
+		} finally {
+			setIsSendReply(false)
+		}
+	}
 
 	return (
 
@@ -62,11 +100,34 @@ export function CommentItem({ comment, onDeleteComment, onLikeComment }: Comment
 
 				{comment.replyCount > 0 &&
 					<span>Ver repostas</span>
+					// TODO: FAZER MESAGEM DE COMMIT
+					// FAZER  GET DAS RESPOSTAS PAGINADSS E VERIFICAÇÃO SE O USUARIO LOGADO JÁ CURTIU OU NÃO A REPOSTA
 				}
 
-				<strong>Responder</strong>
+				<span onClick={() =>  setShowReplyInput(prev => !prev)}>
+					<strong>Responder</strong>
+				</span>
 			</Footer>
 
+			{showReplyInput &&
+				<section>
+					{isSendReply && <p>Enviando ...</p>}
+					<form onSubmit={handleSubmit(inHandleSubmit)}>
+						<textarea
+							{...register("content")}
+							placeholder="Escreva sua resposta..."
+							rows={4}
+						/>
+
+						{errors.content && <span>{errors.content.message}</span>}
+						{replyError && <span>{replyError} </span>}
+
+						<Button text="Enviar" type="submit" />
+					</form>
+
+				</section>
+			}
+			
 		</Container>
 	)
 }
